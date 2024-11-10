ca514d321099dc43c96d7b62044a8b22b15b3c5d
yuki1118 <onedyuki520@gmail.com>
2024-11-10 11:02:55 +0900

    Initial Commit

---
+++ flashMorse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  flashMorse
+//
+//  Created by 吉田侑己 on 2024/11/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ flashMorse/flashMorseApp.swift
@@ -0,0 +1,17 @@
+//
+//  flashMorseApp.swift
+//  flashMorse
+//
+//  Created by 吉田侑己 on 2024/11/10.
+//
+
+import SwiftUI
+
+@main
+struct flashMorseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
